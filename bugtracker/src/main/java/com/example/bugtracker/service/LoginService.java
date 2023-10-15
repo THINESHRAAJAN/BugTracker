e31f5e1cf22bd3032b3976b394a6852b679e8d16
THINESH RAAJAN <140143304+THINESHRAAJAN@users.noreply.github.com>
2023-10-15 22:02:17 +0530

    Add files via upload
    Intit

---
+++ bugtracker/src/main/java/com/example/bugtracker/service/LoginService.java
@@ -0,0 +1,40 @@
+package com.example.bugtracker.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.bugtracker.entity.Login;
+import com.example.bugtracker.repository.LoginRepository;
+
+@Service
+public class LoginService {
+	
+	@Autowired
+	private LoginRepository LRepo;
+	
+	public void save(Login b) {
+		LRepo.save(b);
+	}
+	
+	public List<Login> getAllUsers(){
+		return LRepo.findAll();
+	}
+	
+	public Login getUserById(int id) {
+		return LRepo.findById(id).get();
+	}
+	public void deleteById(int id) {
+		LRepo.deleteById(id);
+	}
+	
+	//Login Validate
+	public Login loginValidate(Login b) {
+		String email = b.getEmail();
+		String password = b.getPassword();
+		return LRepo.getById(b.getId());
+	}
+	
+
+}
